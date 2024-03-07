bf85efd6ff78b26cb1050b22134c25ea0edb82e4
Bhanu Pratap <104151036+BhanuPratapdewangan@users.noreply.github.com>
2024-03-08 02:56:20 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+var copyValue = document.querySelector(".scroll").cloneNode(true);
+document.querySelector("#page2").appendChild(copyValue);
+
+var page7CopyValue = document.querySelector(".container").cloneNode(true);
+document.querySelector("#page7").appendChild(page7CopyValue);
